d6de61658e499e3aebb86d97be4b0b7d32751ad6
kyuuss <adam.czogallik@pm.me>
2025-01-19 22:29:12 +0100

    feat(main): add fps limit

--- main.py
+++ main.py
@@ -10,6 +10,10 @@ def main():
     pygame.init()
     screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
 
+    FPS = 60
+    game_clock = pygame.time.Clock()
+    dt = 0
+
     while True:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
@@ -17,6 +21,8 @@ def main():
 
         screen.fill((0, 0, 0))
         pygame.display.flip()
+
+        dt = game_clock.tick(FPS) / 1000
 
 if __name__ == "__main__":
     main()
